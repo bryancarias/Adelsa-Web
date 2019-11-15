97dce6d8a0f8f3eb740e718c9a658b60018e6c17
Bryan Carias <bcarias84@gmail.com>
2019-11-15 13:52:26 -0600

    Iterfaz home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/public/home/home.component';
+import { AboutComponent } from './components/public/about/about.component';
+import { NavbarComponent } from './components/public/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
